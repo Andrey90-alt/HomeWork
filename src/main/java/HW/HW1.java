d8760b0c5bb17cc3f03ef2ce6a2e06acaa8c7234
Andrey90-alt <ZykovAM@multicarta.ru>
2024-07-21 14:40:55 +0300

    1

--- src/main/java/HW/HW1.java
+++ src/main/java/HW/HW1.java
@@ -10,7 +10,7 @@ public class HW1 {
         checkSign(0,0,0);
         selectColor(11);
         compareNumbers(1,2);
-        addOrSubtractAndPrint(1,2,true);
+        addOrSubtractAndPrint(1,2,false);
     }
     static void greetings() {
         System.out.println("Hello");
@@ -26,11 +26,11 @@ public class HW1 {
         }
     }
     static void selectColor(int data){
-        if (data<10){
+        if (data<=10){
             System.out.println("Красный");
-        } else if (data>10 && data<20 ) {
+        } else if (data<=20) {
             System.out.println("Желтый");
-        } else if (data>20) {
+        } else {
             System.out.println("Зеленый");
         }
     }
@@ -42,7 +42,7 @@ public class HW1 {
         }
     }
     static void addOrSubtractAndPrint(int initValue, int delta, boolean increment){
-        if (increment = true){
+        if (!increment){
             int result = initValue + delta;
             System.out.println(result);
         }else {
