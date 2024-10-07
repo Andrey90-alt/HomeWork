ccc5d11c8d04cb956254eb9fe57f2bb14cd6ca91
Andrey <ZykovAM@multicarta.ru>
2024-10-07 19:21:09 +0300

    1

--- src/main/java/hw/hw12/Hw12.java
+++ src/main/java/hw/hw12/Hw12.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Hw12 {
     public  void readFile(String filename) {
         try {
-            File myFile = new File("C:\\Users\\Андрей\\IdeaProjects\\HomeWork\\src\\main\\java\\hw\\hw12\\files\\"+filename);
+            File myFile = new File("src/main/java/hw/hw12/files/"+filename);
             Scanner myReader = new Scanner(myFile);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
@@ -21,7 +21,7 @@ public class Hw12 {
 
 
     public  void createFile(String filename) {
-        File myObj = new File("C:\\Users\\Андрей\\IdeaProjects\\HomeWork\\src\\main\\java\\hw\\hw12\\files\\"+filename);
+        File myObj = new File("src/main/java/hw/hw12/files/"+filename);
         try {
             if (myObj.createNewFile()) {
                 System.out.println("Фаил создан: " + myObj.getName());
@@ -35,7 +35,7 @@ public class Hw12 {
 
     public  void writeFile(String filename) {
         Scanner in = new Scanner(System.in);
-        File myFile = new File("C:\\Users\\Андрей\\IdeaProjects\\HomeWork\\src\\main\\java\\hw\\hw12\\files\\"+filename);
+        File myFile = new File("src/main/java/hw/hw12/files/"+filename);
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(myFile, true));
             System.out.println("Введите строку");
@@ -55,8 +55,8 @@ public class Hw12 {
         Hw12 hw12 = new Hw12();
         Scanner in = new Scanner(System.in);
         try {
-            File f = new File("C:\\Users\\Андрей\\IdeaProjects\\HomeWork\\src\\main\\java\\hw\\hw12\\files");
-            File[] files = f.listFiles();
+            File file = new File("src/main/java/hw/hw12/files/");
+            File[] files = file.listFiles();
             System.out.println("Фаилы в директории:");
             for (int i = 0; i < files.length; i++) {
                 System.out.println(files[i].getName());
