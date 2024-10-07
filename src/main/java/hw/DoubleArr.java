e7f59639002993f4303cfe9f43f3cc49887b91e2
Andrey <ZykovAM@multicarta.ru>
2024-10-07 19:25:57 +0300

    1

--- src/main/java/hw/DoubleArr.java
+++ src/main/java/hw/DoubleArr.java
@@ -22,22 +22,22 @@ public class DoubleArr {
         double[] array = new double[100000000];
         long startTime = System.currentTimeMillis();
         Thread t1 = new Thread(() -> {
-            for (int i=0; i<array[25000000];i++){
+            for (int i=0; i<25000000;i++){
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
         Thread t2 = new Thread(() -> {
-            for (int i = 0; i > array[25000000] && i < array[50000000]; i++){
+            for (int i = 0; i > 25000000 && i < 50000000; i++){
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
         Thread t3 = new Thread(() -> {
-            for (int i=0; i > array[50000000] && i < array[75000000];i++){
+            for (int i=0; i > 50000000 && i < 75000000;i++){
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
         Thread t4 = new Thread(() -> {
-            for (int i=0; i > array[75000000] && i < array[99999999];i++){
+            for (int i=0; i > 75000000 && i < array.length;i++){
                 array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
             }
         });
